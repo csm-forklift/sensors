452c710b7e4563123a046ea8878f4c1e07208aa5
larsenkw <kylew.larsen@gmail.com>
2018-09-25 21:41:37 -0600

    updated 'encoder_read.py' to now be ROS compatible

--- src/encoder_read.py
+++ src/encoder_read.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 ''' This is the node for reading the encoder raw values and publishing them. 
 The tick values from this node are read by encoder_odom and converted into 
 odometry messages.'''
@@ -5,264 +7,236 @@ odometry messages.'''
 import time
 import RPi.GPIO as GPIO
 import rospy
+from std_msgs.msg import Int16
 
-def setup():
-    global ir1_pin
-    global ir2_pin
-    global ir3_pin
-    global ir4_pin
-    global switch_pin
+class EncoderRead():
+    def __init__(self):
+        # Define Global Variables
+        self.counter1 = Int16(0)
+        self.counter2 = Int16(0)
+
+        self.ir1 = 0
+        self.ir2 = 0
+        self.ir3 = 0
+        self.ir4 = 0
+
+        self.oldenc_1 = 0
+        self.oldenc_2 = 0
+        self.oldenc_3 = 0
+        self.oldenc_4 = 0
+
+        self.state1 = 0
+        self.oldstate1 = 0
+        self.state2 = 0
+        self.oldstate2 = 0
+
+        self.ir1_pin = 29
+        self.ir2_pin = 31
+        self.ir3_pin = 33
+        self.ir4_pin = 35
+        self.switch_pin = 37
+        
+        # Create publishers
+        self.rate = 60 # Hertz
+        rospy.init_node("encoder_read")
+        self.right_pub = rospy.Publisher('enc/front_wheel_right_ticks', Int16, queue_size=10);
+        self.left_pub = rospy.Publisher('enc/front_wheel_left_ticks', Int16, queue_size=10);
+        
+        self.setup()
+
+
+    def setup(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.ir1_pin, GPIO.IN)
+        GPIO.setup(self.ir2_pin, GPIO.IN)
+        GPIO.setup(self.ir3_pin, GPIO.IN)
+        GPIO.setup(self.ir4_pin, GPIO.IN)
+        GPIO.setup(self.switch_pin, GPIO.OUT)
+
+        GPIO.output(self.switch_pin, 1)
+        time.sleep(2)
+
+        if GPIO.input(self.ir1_pin):
+            self.ir1 = 1
+        else:
+            self.ir1 = 0
+        if GPIO.input(self.ir2_pin):
+            self.ir2 = 1
+        else:
+            self.ir2 = 0
+        if GPIO.input(self.ir3_pin):
+            self.ir3 = 1
+        else:
+            self.ir3 = 0
+        if GPIO.input(self.ir4_pin):
+            self.ir4 = 1
+        else:
+            self.ir4 = 0
+
+        # state initialization
+        if self.ir1 == 0:
+            if self.ir2 == 0:
+                self.oldstate1 = 0
+            else:
+                self.oldstate1 = 1
+        else:
+            if self.ir2 == 0:
+                self.oldstate1 = 2
+            else:
+                self.oldstate1 = 3
+
+        if self.ir3 == 0:
+            if self.ir4 == 0:
+                self.oldstate2 = 0
+            else:
+                self.oldstate2 = 1
+        else:
+            if self.ir4 == 0:
+                self.oldstate1 = 2
+            else:
+                self.oldstate2 = 3
+                
+
+    def loop(self):
+        GPIO.output(self.switch_pin, 1)
+
+        if GPIO.input(self.ir1_pin):
+            self.ir1 = 1
+        else:
+            self.ir1 = 0
+        if GPIO.input(self.ir2_pin):
+            self.ir2 = 1
+        else:
+            self.ir2 = 0
+        if GPIO.input(self.ir3_pin):
+            self.ir3 = 1
+        else:
+            self.ir3 = 0
+        if GPIO.input(self.ir4_pin):
+            self.ir4 = 1
+        else:
+            self.ir4 = 0
+
+        if self.ir1 == 0:
+            if self.ir2 == 0:
+                self.state1 = 0
+            else:
+                self.state1 = 1
+        else:
+            if self.ir2 == 0:
+                self.state1 = 2
+            else:
+                self.state1 = 3
+
+        if self.ir3 == 0:
+            if self.ir4 == 0:
+                self.state2 = 0
+            else:
+                self.state2 = 1
+        else:
+            if self.ir4 == 0:
+                self.state2 = 2
+            else:
+                self.state2 = 3
+
+        # main state function
+        if self.oldstate1 == 0:
+            if self.state1 == 1:
+                self.returnvalue1 = 1
+            elif self.state1 == 2:
+                self.returnvalue1 = -1
+            else:
+                self.returnvalue1 = 0
+
+        elif self.oldstate1 == 1:
+            if self.state1 == 3:
+                self.returnvalue1 = 1
+            elif self.state1 == 0:
+                self.returnvalue1 = -1
+            else:
+                self.returnvalue1 = 0
+
+        elif self.oldstate1 == 2:
+            if self.state1 == 0:
+                self.returnvalue1 = 1
+            elif self.state1 == 3:
+                self.returnvalue1 = -1
+            else:
+                self.returnvalue1 = 0
+
+        elif self.oldstate1 == 3:
+            if self.state1 == 2:
+                self.returnvalue1 = 1
+            elif self.state1 == 1:
+                self.returnvalue1 = -1
+            else:
+                self.returnvalue1 = 0
+
+        ####
+
+        if self.oldstate2 == 0:
+            if self.state2 == 1:
+                self.returnvalue2 = 1
+            elif self.state2 == 2:
+                self.returnvalue2 = -1
+            else:
+                self.returnvalue2 = 0
+
+        elif self.oldstate2 == 1:
+            if self.state2 == 3:
+                self.returnvalue2 = 1
+            elif self.state2 == 0:
+                self.returnvalue2 = -1
+            else:
+                self.returnvalue2 = 0
+
+        elif self.oldstate2 == 2:
+            if self.state2 == 0:
+                self.returnvalue2 = 1
+            elif self.state2 == 3:
+                self.returnvalue2 = -1
+            else:
+                self.returnvalue2 = 0
+
+        elif self.oldstate2 == 3:
+            if self.state2 == 2:
+                self.returnvalue2 = 1
+            elif self.state2 == 1:
+                self.returnvalue2 = -1
+            else:
+                self.returnvalue2 = 0
+
+        if self.ir1 != self.oldenc_1:
+            self.counter1 = self.counter1 + self.returnvalue1
+        if self.ir3 != self.oldenc_3:
+            self.counter2 = self.counter2 + self.returnvalue2
+
+        # Publish counter values
+        #print("counter1: %d, counter2: %d" % (counter1, counter2))
+        self.right_pub.publish(self.counter1);
+        self.left_pub.publish(self.counter2);
+
+        # Update states
+        self.oldenc_1 = self.ir1
+        self.oldenc_2 = self.ir2
+        self.oldenc_3 = self.ir3
+        self.oldenc_4 = self.ir4
+
+        self.oldstate1 = self.state1
+        self.oldstate2 = self.state2
     
-    global ir1
-    global ir2
-    global ir3
-    global ir4
-    
-    global oldstate1
-    global oldstate2
-
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(ir1_pin, GPIO.IN)
-    GPIO.setup(ir2_pin, GPIO.IN)
-    GPIO.setup(ir3_pin, GPIO.IN)
-    GPIO.setup(ir4_pin, GPIO.IN)
-    GPIO.setup(switch_pin, GPIO.OUT)
-
-    GPIO.output(switch_pin, 1)
-    time.sleep(2)
-
-    if GPIO.input(ir1_pin):
-        ir1 = 1
-    else:
-        ir1 = 0
-    if GPIO.input(ir2_pin):
-        ir2 = 1
-    else:
-        ir2 = 0
-    if GPIO.input(ir3_pin):
-        ir3 = 1
-    else:
-        ir3 = 0
-    if GPIO.input(ir4_pin):
-        ir4 = 1
-    else:
-        ir4 = 0
-
-    # state initialization
-    if ir1 == 0:
-        if ir2 == 0:
-            oldstate1 = 0
-        else:
-            oldstate1 = 1
-    else:
-        if ir2 == 0:
-            oldstate1 = 2
-        else:
-            oldstate1 = 3
-
-    if ir3 == 0:
-        if ir4 == 0:
-            oldstate2 = 0
-        else:
-            oldstate2 = 1
-    else:
-        if ir4 == 0:
-            oldstate1 = 2
-        else:
-            oldstate2 = 3
-
-    # ROS stuff goes here
-    # and IMU stuff
-
-############################
-
-
-def loop():
-    global ir1_pin
-    global ir2_pin
-    global ir3_pin
-    global ir4_pin
-    global switch_pin
-    
-    global ir1
-    global ir2
-    global ir3
-    global ir4
-    
-    global oldenc_1
-    global oldenc_2
-    global oldenc_3
-    global oldenc_4
-
-    global state1
-    global state2
-
-    global oldstate1
-    global oldstate2
-
-    global counter1
-    global counter2
-
-    GPIO.output(switch_pin, 1)
-
-    if GPIO.input(ir1_pin):
-        ir1 = 1
-    else:
-        ir1 = 0
-    if GPIO.input(ir2_pin):
-        ir2 = 1
-    else:
-        ir2 = 0
-    if GPIO.input(ir3_pin):
-        ir3 = 1
-    else:
-        ir3 = 0
-    if GPIO.input(ir4_pin):
-        ir4 = 1
-    else:
-        ir4 = 0
-
-    if ir1 == 0:
-        if ir2 == 0:
-            state1 = 0
-        else:
-            state1 = 1
-    else:
-        if ir2 == 0:
-            state1 = 2
-        else:
-            state1 = 3
-
-    if ir3 == 0:
-        if ir4 == 0:
-            state2 = 0
-        else:
-            state2 = 1
-    else:
-        if ir4 == 0:
-            state2 = 2
-        else:
-            state2 = 3
-
-    # main state function
-    if oldstate1 == 0:
-        if state1 == 1:
-            returnvalue1 = 1
-        elif state1 == 2:
-            returnvalue1 = -1
-        else:
-            returnvalue1 = 0
-
-    elif oldstate1 == 1:
-        if state1 == 3:
-            returnvalue1 = 1
-        elif state1 == 0:
-            returnvalue1 = -1
-        else:
-            returnvalue1 = 0
-
-    elif oldstate1 == 2:
-        if state1 == 0:
-            returnvalue1 = 1
-        elif state1 == 3:
-            returnvalue1 = -1
-        else:
-            returnvalue1 = 0
-
-    elif oldstate1 == 3:
-        if state1 == 2:
-            returnvalue1 = 1
-        elif state1 == 1:
-            returnvalue1 = -1
-        else:
-            returnvalue1 = 0
-
-    ####
-
-    if oldstate2 == 0:
-        if state2 == 1:
-            returnvalue2 = 1
-        elif state2 == 2:
-            returnvalue2 = -1
-        else:
-            returnvalue2 = 0
-
-    elif oldstate2 == 1:
-        if state2 == 3:
-            returnvalue2 = 1
-        elif state2 == 0:
-            returnvalue2 = -1
-        else:
-            returnvalue2 = 0
-
-    elif oldstate2 == 2:
-        if state2 == 0:
-            returnvalue2 = 1
-        elif state2 == 3:
-            returnvalue2 = -1
-        else:
-            returnvalue2 = 0
-
-    elif oldstate2 == 3:
-        if state2 == 2:
-            returnvalue2 = 1
-        elif state2 == 1:
-            returnvalue2 = -1
-        else:
-            returnvalue2 = 0
-
-    if ir1 != oldenc_1:
-        counter1 = counter1 + returnvalue1
-    if ir3 != oldenc_3:
-        counter2 = counter2 + returnvalue2
-
-    print("counter1: %d, counter2: %d" % (counter1, counter2))
-
-    oldenc_1 = ir1
-    oldenc_2 = ir2
-    oldenc_3 = ir3
-    oldenc_4 = ir4
-
-    oldstate1 = state1
-    oldstate2 = state2
-
-
-# Main Function
-counter1 = 0
-counter2 = 0
-
-ir1 = 0
-ir2 = 0
-ir3 = 0
-ir4 = 0
-
-oldenc_1 = 0
-oldenc_2 = 0
-oldenc_3 = 0
-oldenc_4 = 0
-
-state1 = 0
-oldstate1 = 0
-state2 = 0
-oldstate2 = 0
-
-ir1_pin = 29
-ir2_pin = 31
-ir3_pin = 33
-ir4_pin = 35
-switch_pin = 37
-
+    def spin(self):
+        r = rospy.Rate(self.rate)
+        while (not rospy.is_shutdown()):
+            self.loop()
+            r.sleep()
+            
 
 
 def main():
-    rospy.init_node("encoder_read")
-    
-    print "Beginning setup function"
-    setup()
     try:
-        print "Entering loop..."
-        while True:
-            loop()
+        encoder_read = EncoderRead()
+        encoder_read.spin()
     finally:
         print "Cleaning up pins."
         GPIO.cleanup()
