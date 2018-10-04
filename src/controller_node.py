40943d2f391a0f414d9130c299db3874e7f1df8a
larsenkw <kylew.larsen@gmail.com>
2018-10-03 20:51:48 -0600

    created controller_node to handle all outputs for the Pi

---
+++ src/controller_node.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+''' This node manages all the control signals sent from the Raspberry Pi to
+manipulate actuators. Currently this includes braking, acceleration, and
+steering.'''
+
+
+import rospy
+from std_msgs.msg import Float32, Float64
+import RPI.GPIO as gpio
+import time
+
+
+class ControllerNode():
+    def __init__(self):
+        rospy.init_node("controller_node")
+        self.rate = 60
+
+        # Commands
+        self.brake_pub = rospy.Publisher("controls/brake/desired", Float32, queue_size=10)
+        self.steering_sub = rospy.Subscriber("controls/steering/input", Float64, self.steering_callback)
+        self.accelerator_sub = rospy.Subscriber("controls/accelerator/input", Float64, self.accelerator_callback)
+
+        # GPIO Pin Setup
+        self.ACCELERATOR_PIN = 37
+        gpio.setmode(gpio.BOARD)
+        self.accelerator_pwm = gpio.PWM(self.ACCELERATOR_PIN, 1000)
+
+    def spin(self):
+        r = rospy.Rate(self.rate)
+        while not rospy.is_shutdown():
+            self.update()
+            r.sleep()
+
+    def update(self):
+        for i in range(0,101):
+            self.accelerator_pwm.start(i)
+            time.sleep(0.05)
+
+        self.accelerator_pwm.stop()
+        time.sleep(1)
+
+        for i in range(100, 0, -1):
+            self.accelerator_pwm.start(i)
+            time.sleep(0.05)
+
+    def steering_callback(self, msg):
+        pass
+
+    def accelerator_callback(self, msg):
+        pass
+
+if __name__ == '__main__':
+    try:
+        controller_node = ControllerNode()
+        controller_node.spin()
+    except rospy.ROSInterruptException:
+        pass
+    finally:
+        gpio.cleanup()
