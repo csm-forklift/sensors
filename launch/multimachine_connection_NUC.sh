1463b79ce2ea51c21321f3c6fc927f725cea6bb3
larsenkw <kylew.larsen@gmail.com>
2018-11-13 14:07:54 -0700

    added launch file for running all of the control nodes on the Raspberry Pi

---
+++ launch/multimachine_connection_NUC.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# NUC Version
+
+# This script sets up the ROS environment variables needed to allow the Raspberry
+# Pi and NUC to connect together. You must set the ROS master URI as the same
+# value on each computer that needs to be connected. Then for any computer that
+# will be publishing values must export the ROS_IP variable to be their IP address.
+
+export ROS_MASTER_URI=http://forkPC.local:11311
+export ROS_IP=192.168.1.1
