c65cd675db3f8d3d29a0bf6c880c8a85d0d61b30
csm-forklift <klarsen@mymail.mines.edu>
2019-05-25 18:49:16 -0600

    updated slcan_add.sh to read default device 'ttyUSB0' without having to specify an argument

--- launch/slcan_add.sh
+++ launch/slcan_add.sh
@@ -5,8 +5,17 @@
 echo "Starting CANUSB connection on 'slcand'"
 /usr/bin/logger "Starting CANUSB connection on 'slcand'"
 
+# Get first argument and store it as device name
+# if empty, use default
+if [ $# -eq 0 ]
+then
+    DEVICE_NAME="ttyUSB0"
+else
+    DEVICE_NAME=$1
+fi  
+
 # -s4 argument sets rate to 125K bps
-slcand -o -c -f -s4 /dev/$1 slcan0 # this version takes the first argument to the script
+slcand -o -c -f -s4 /dev/$DEVICE_NAME slcan0 # this version takes the first argument to the script
 # slcand -o -c -f -s4 /dev/ttyUSB0 slcan0 # this version lets you manually set the device location
 
 echo "Sleep for 2 seconds"
