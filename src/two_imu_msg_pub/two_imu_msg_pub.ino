1f157b75bb6da68842ef879f0da258dd8d7d2c09
larsenkw <kylew.larsen@gmail.com>
2019-06-08 09:29:42 -0600

    updated IMU calibrations

--- src/two_imu_msg_pub/two_imu_msg_pub.ino
+++ src/two_imu_msg_pub/two_imu_msg_pub.ino
@@ -8,16 +8,16 @@
  * https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/arduino-code
  * 
  * Board Connection
- * MEGA      BNO055
+ * MEGA      BNO055 (same pins for both because they share I2C lines)
  * 5V        Vin
  * GND       GND
  * D21       SCL
  * D22       SDA
  * 
- * IMU 1
+ * IMU 0
  * ID on back: 4517
  * 
- * IMU 2
+ * IMU 1
  * ID on back: 0519
  */
 
@@ -36,26 +36,26 @@
 
 // ROS Objects
 ros::NodeHandle nh;
-sensor_msgs::Imu imu_data;
-ros::Publisher imu_pub("arduino/imu", &imu_data);
-sensors::ImuCalibStatus imu_calib_status;
-ros::Publisher imu_status_pub("arduino/imu_status", &imu_calib_status);
-sensor_msgs::Imu imu2_data;
-ros::Publisher imu2_pub("arduino/imu2", &imu2_data);
-sensors::ImuCalibStatus imu2_calib_status;
-ros::Publisher imu2_status_pub("arduino/imu2_status", &imu2_calib_status);
+sensor_msgs::Imu imu0_data;
+ros::Publisher imu0_pub("arduino/imu0", &imu0_data);
+sensors::ImuCalibStatus imu0_calib_status;
+ros::Publisher imu0_status_pub("arduino/imu0_status", &imu0_calib_status);
+sensor_msgs::Imu imu1_data;
+ros::Publisher imu1_pub("arduino/imu1", &imu1_data);
+sensors::ImuCalibStatus imu1_calib_status;
+ros::Publisher imu1_status_pub("arduino/imu1_status", &imu1_calib_status);
 
 // IMU Objects
-Adafruit_BNO055 bno = Adafruit_BNO055(55);
-Adafruit_BNO055 bno2 = Adafruit_BNO055(56, BNO055_ADDRESS_B);
-imu::Quaternion quat;
-imu::Vector<3> ang;
-imu::Vector<3> lin;
-imu::Vector<3> mag;
-imu::Quaternion quat2;
-imu::Vector<3> ang2;
-imu::Vector<3> lin2;
-imu::Vector<3> mag2;
+Adafruit_BNO055 bno0 = Adafruit_BNO055(55);
+Adafruit_BNO055 bno1 = Adafruit_BNO055(56, BNO055_ADDRESS_B);
+imu::Quaternion quat0;
+imu::Vector<3> ang0;
+imu::Vector<3> lin0;
+imu::Vector<3> mag0;
+imu::Quaternion quat1;
+imu::Vector<3> ang1;
+imu::Vector<3> lin1;
+imu::Vector<3> mag1;
 // comment out the three vectors above and use this variable, 'vec',
 // for each of the three vector values you read if you are low on memory.
 //imu::Vector<3> vec;
@@ -66,10 +66,10 @@ const int DEBUG_LED = 13;
 void setup() {
   // Start ROS Node
   nh.initNode();
-  nh.advertise(imu_pub);
-  nh.advertise(imu_status_pub);
-  nh.advertise(imu2_pub);
-  nh.advertise(imu2_status_pub);
+  nh.advertise(imu0_pub);
+  nh.advertise(imu0_status_pub);
+  nh.advertise(imu1_pub);
+  nh.advertise(imu1_status_pub);
   Serial.begin(57600);
   delay(100);
 
@@ -78,7 +78,7 @@ void setup() {
   digitalWrite(DEBUG_LED, LOW);
 
   // Wait for BNO055 to begin before continuing
-  if (!bno.begin()) {
+  if (!bno0.begin()) {
     //Serial.println("No BNO055 detected.");
     // Blinks twice a second if no connection is made
     while(1) {
@@ -94,7 +94,7 @@ void setup() {
   }
 
   // Wait for second BNO055 to begin before continuing
-  if (!bno2.begin()) {
+  if (!bno1.begin()) {
     // Blinks four times a second if no connection is made
     while(1) {
       digitalWrite(DEBUG_LED, HIGH);
@@ -106,9 +106,9 @@ void setup() {
 
   // Get Default Calibration Parameters
   adafruit_bno055_offsets_t calibration_data;
-  bno.getSensorOffsets(calibration_data);
+  bno0.getSensorOffsets(calibration_data);
 
-  // Grab IMU calibration parameters, if parameter is not set, use default value
+  // Grab IMU0 calibration parameters, if parameter is not set, use default value
   int16_t accel_offset_x;
   int16_t accel_offset_y;
   int16_t accel_offset_z;
@@ -124,37 +124,37 @@ void setup() {
   // Needs a delay before attempting to load parameters
   delay(1000);
   
-  if (!nh.getParam("imu/calibration/accel_offset_x", &accel_offset_x)) {
+  if (!nh.getParam("imu0/calibration/accel_offset_x", &accel_offset_x)) {
     accel_offset_x = calibration_data.accel_offset_x;
   }
-  if (!nh.getParam("imu/calibration/accel_offset_y", &accel_offset_y)) {
+  if (!nh.getParam("imu0/calibration/accel_offset_y", &accel_offset_y)) {
     accel_offset_y = calibration_data.accel_offset_y;
   }
-  if (!nh.getParam("imu/calibration/accel_offset_z", &accel_offset_z)) {
+  if (!nh.getParam("imu0/calibration/accel_offset_z", &accel_offset_z)) {
     accel_offset_z = calibration_data.accel_offset_z;
   }
-  if (!nh.getParam("imu/calibration/accel_radius", &accel_radius)) {
+  if (!nh.getParam("imu0/calibration/accel_radius", &accel_radius)) {
     accel_radius = calibration_data.accel_radius;
   }
-  if (!nh.getParam("imu/calibration/gyro_offset_x", &gyro_offset_x)) {
+  if (!nh.getParam("imu0/calibration/gyro_offset_x", &gyro_offset_x)) {
     gyro_offset_x = calibration_data.gyro_offset_x;
   }
-  if (!nh.getParam("imu/calibration/gyro_offset_y", &gyro_offset_y)) {
+  if (!nh.getParam("imu0/calibration/gyro_offset_y", &gyro_offset_y)) {
     gyro_offset_y = calibration_data.gyro_offset_y;
   }
-  if (!nh.getParam("imu/calibration/gyro_offset_z", &gyro_offset_z)) {
+  if (!nh.getParam("imu0/calibration/gyro_offset_z", &gyro_offset_z)) {
     gyro_offset_z = calibration_data.gyro_offset_z;
   }
-  if (!nh.getParam("imu/calibration/mag_offset_x", &mag_offset_x)) {
+  if (!nh.getParam("imu0/calibration/mag_offset_x", &mag_offset_x)) {
     mag_offset_x = calibration_data.mag_offset_x;
   }
-  if (!nh.getParam("imu/calibration/mag_offset_y", &mag_offset_y)) {
+  if (!nh.getParam("imu0/calibration/mag_offset_y", &mag_offset_y)) {
     mag_offset_y = calibration_data.mag_offset_y;
   }
-  if (!nh.getParam("imu/calibration/mag_offset_z", &mag_offset_z)) {
+  if (!nh.getParam("imu0/calibration/mag_offset_z", &mag_offset_z)) {
     mag_offset_z = calibration_data.mag_offset_z;
   }
-  if (!nh.getParam("imu/calibration/mag_radius", &mag_radius)) {
+  if (!nh.getParam("imu0/calibration/mag_radius", &mag_radius)) {
     mag_radius = calibration_data.mag_radius;
   }
 
@@ -170,46 +170,46 @@ void setup() {
   calibration_data.mag_offset_y = mag_offset_y;
   calibration_data.mag_offset_z = mag_offset_z;
   calibration_data.mag_radius = mag_radius;
-  bno.setSensorOffsets(calibration_data);
+  bno0.setSensorOffsets(calibration_data);
   
-  // Load Calibration Parameters for IMU 2
-  bno2.getSensorOffsets(calibration_data); // grab defaults first
+  // Load Calibration Parameters for IMU 1
+  bno1.getSensorOffsets(calibration_data); // grab defaults first
   
-  if (!nh.getParam("imu2/calibration/accel_offset_x", &accel_offset_x)) {
+  if (!nh.getParam("imu1/calibration/accel_offset_x", &accel_offset_x)) {
     accel_offset_x = calibration_data.accel_offset_x;
   }
-  if (!nh.getParam("imu2/calibration/accel_offset_y", &accel_offset_y)) {
+  if (!nh.getParam("imu1/calibration/accel_offset_y", &accel_offset_y)) {
     accel_offset_y = calibration_data.accel_offset_y;
   }
-  if (!nh.getParam("imu2/calibration/accel_offset_z", &accel_offset_z)) {
+  if (!nh.getParam("imu1/calibration/accel_offset_z", &accel_offset_z)) {
     accel_offset_z = calibration_data.accel_offset_z;
   }
-  if (!nh.getParam("imu2/calibration/accel_radius", &accel_radius)) {
+  if (!nh.getParam("imu1/calibration/accel_radius", &accel_radius)) {
     accel_radius = calibration_data.accel_radius;
   }
-  if (!nh.getParam("imu2/calibration/gyro_offset_x", &gyro_offset_x)) {
+  if (!nh.getParam("imu1/calibration/gyro_offset_x", &gyro_offset_x)) {
     gyro_offset_x = calibration_data.gyro_offset_x;
   }
-  if (!nh.getParam("imu2/calibration/gyro_offset_y", &gyro_offset_y)) {
+  if (!nh.getParam("imu1/calibration/gyro_offset_y", &gyro_offset_y)) {
     gyro_offset_y = calibration_data.gyro_offset_y;
   }
-  if (!nh.getParam("imu2/calibration/gyro_offset_z", &gyro_offset_z)) {
+  if (!nh.getParam("imu1/calibration/gyro_offset_z", &gyro_offset_z)) {
     gyro_offset_z = calibration_data.gyro_offset_z;
   }
-  if (!nh.getParam("imu2/calibration/mag_offset_x", &mag_offset_x)) {
+  if (!nh.getParam("imu1/calibration/mag_offset_x", &mag_offset_x)) {
     mag_offset_x = calibration_data.mag_offset_x;
   }
-  if (!nh.getParam("imu2/calibration/mag_offset_y", &mag_offset_y)) {
+  if (!nh.getParam("imu1/calibration/mag_offset_y", &mag_offset_y)) {
     mag_offset_y = calibration_data.mag_offset_y;
   }
-  if (!nh.getParam("imu2/calibration/mag_offset_z", &mag_offset_z)) {
+  if (!nh.getParam("imu1/calibration/mag_offset_z", &mag_offset_z)) {
     mag_offset_z = calibration_data.mag_offset_z;
   }
-  if (!nh.getParam("imu2/calibration/mag_radius", &mag_radius)) {
+  if (!nh.getParam("imu1/calibration/mag_radius", &mag_radius)) {
     mag_radius = calibration_data.mag_radius;
   }
 
-  // Upload parameters to IMU 2
+  // Upload parameters to IMU 1
   calibration_data.accel_offset_x = accel_offset_x;
   calibration_data.accel_offset_y = accel_offset_y;
   calibration_data.accel_offset_z = accel_offset_z;
@@ -221,75 +221,75 @@ void setup() {
   calibration_data.mag_offset_y = mag_offset_y;
   calibration_data.mag_offset_z = mag_offset_z;
   calibration_data.mag_radius = mag_radius;
-  bno2.setSensorOffsets(calibration_data);
+  bno1.setSensorOffsets(calibration_data);
 }
 
 void loop() {
-  // Get IMU data from sensor
-  quat = bno.getQuat();
-  ang = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  lin = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  mag = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  // Get IMU0 data from sensor
+  quat0 = bno0.getQuat();
+  ang0 = bno0.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  lin0 = bno0.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  mag0 = bno0.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
 
   // Set header
-  imu_data.header.stamp = nh.now();
-  imu_data.header.frame_id = "imu_link";
+  imu0_data.header.stamp = nh.now();
+  imu0_data.header.frame_id = "imu0_link";
 
   // Set orientation
-  imu_data.orientation.x = quat.x();
-  imu_data.orientation.y = quat.y();
-  imu_data.orientation.z = quat.z();
-  imu_data.orientation.w = quat.w();
+  imu0_data.orientation.x = quat0.x();
+  imu0_data.orientation.y = quat0.y();
+  imu0_data.orientation.z = quat0.z();
+  imu0_data.orientation.w = quat0.w();
 
   // Set angular velocity
-  imu_data.angular_velocity.x = ang.x();
-  imu_data.angular_velocity.y = ang.y();
-  imu_data.angular_velocity.z = ang.z();
+  imu0_data.angular_velocity.x = ang0.x();
+  imu0_data.angular_velocity.y = ang0.y();
+  imu0_data.angular_velocity.z = ang0.z();
 
   // Set linear acceleration
-  imu_data.linear_acceleration.x = lin.x();
-  imu_data.linear_acceleration.y = lin.y();
-  imu_data.linear_acceleration.z = lin.z();
+  imu0_data.linear_acceleration.x = lin0.x();
+  imu0_data.linear_acceleration.y = lin0.y();
+  imu0_data.linear_acceleration.z = lin0.z();
 
   // Grab calibration status
-  bno.getCalibration(&imu_calib_status.system, &imu_calib_status.gyro, 
-                     &imu_calib_status.accel, &imu_calib_status.mag);
+  bno0.getCalibration(&imu0_calib_status.system, &imu0_calib_status.gyro, 
+                     &imu0_calib_status.accel, &imu0_calib_status.mag);
 
-  // Get Second IMU data from sensor
-  quat2 = bno2.getQuat();
-  ang2 = bno2.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  lin2 = bno2.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  mag2 = bno2.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  // Get IMU1 data from sensor
+  quat1 = bno1.getQuat();
+  ang1 = bno1.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  lin1 = bno1.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  mag1 = bno1.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
 
   // Set header
-  imu2_data.header.stamp = nh.now();
-  imu2_data.header.frame_id = "imu2_link";
+  imu1_data.header.stamp = nh.now();
+  imu1_data.header.frame_id = "imu1_link";
 
   // Set orientation
-  imu2_data.orientation.x = quat2.x();
-  imu2_data.orientation.y = quat2.y();
-  imu2_data.orientation.z = quat2.z();
-  imu2_data.orientation.w = quat2.w();
+  imu1_data.orientation.x = quat1.x();
+  imu1_data.orientation.y = quat1.y();
+  imu1_data.orientation.z = quat1.z();
+  imu1_data.orientation.w = quat1.w();
 
   // Set angular velocity
-  imu2_data.angular_velocity.x = ang2.x();
-  imu2_data.angular_velocity.y = ang2.y();
-  imu2_data.angular_velocity.z = ang2.z();
+  imu1_data.angular_velocity.x = ang1.x();
+  imu1_data.angular_velocity.y = ang1.y();
+  imu1_data.angular_velocity.z = ang1.z();
 
   // Set linear acceleration
-  imu2_data.linear_acceleration.x = lin2.x();
-  imu2_data.linear_acceleration.y = lin2.y();
-  imu2_data.linear_acceleration.z = lin2.z();
+  imu1_data.linear_acceleration.x = lin1.x();
+  imu1_data.linear_acceleration.y = lin1.y();
+  imu1_data.linear_acceleration.z = lin1.z();
 
   // Grab calibration status
-  bno2.getCalibration(&imu2_calib_status.system, &imu2_calib_status.gyro, 
-                     &imu2_calib_status.accel, &imu2_calib_status.mag);
+  bno1.getCalibration(&imu1_calib_status.system, &imu1_calib_status.gyro, 
+                     &imu1_calib_status.accel, &imu1_calib_status.mag);
 
   // Publish messages
-  imu_pub.publish(&imu_data);
-  imu_status_pub.publish(&imu_calib_status);
-  imu2_pub.publish(&imu2_data);
-  imu2_status_pub.publish(&imu2_calib_status);
+  imu0_pub.publish(&imu0_data);
+  imu0_status_pub.publish(&imu0_calib_status);
+  imu1_pub.publish(&imu1_data);
+  imu1_status_pub.publish(&imu1_calib_status);
 
   nh.spinOnce();
   delay(1);
