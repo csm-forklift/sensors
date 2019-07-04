2bf853ed4e3ec5070cfb8655cc38514ecfe340fc
larsenkw <kylew.larsen@gmail.com>
2019-07-03 23:13:13 -0600

    added option for Twist or TwistWithCovarianceStamped

--- src/velocity_conversion.cpp
+++ src/velocity_conversion.cpp
@@ -8,6 +8,7 @@
 #include <std_msgs/Float64.h>
 #include <std_msgs/Int8.h>
 #include <geometry_msgs/TwistWithCovarianceStamped.h>
+#include <geometry_msgs/Twist.h>
 #include <cmath>
 #include <string>
 
@@ -19,11 +20,12 @@ private:
     ros::NodeHandle nh_;
     ros::Subscriber velocity_sub; // grabs velocity in m/s
     ros::Subscriber steering_angle_sub; // grabs steering angle in rad
-    ros::Subscriber gear_sub; // gets driving direction
+    ros::Publisher twist_with_cov_pub; // publishes combined linear and angular velocities with covariance
     ros::Publisher twist_pub; // publishes combined linear and angular velocities
     ros::Rate rate;
     double publish_frequency; // desired publishing rate
     std::string twist_frame; // frame id of the twist
+    bool use_covariance; // if true, uses the TwistWithCovarianceStamped message, else uses Twist
 
     //----- Parameters
     // Forklift Dimensions
@@ -33,30 +35,35 @@ private:
     // Motion variables
     double velocity; // m/s
     double angle; // rad
-    int gear; // 1 = forward, 0 = neutral, -1 = reverse
-    geometry_msgs::TwistWithCovarianceStamped twist;
+    geometry_msgs::TwistWithCovarianceStamped twist_with_cov;
+    geometry_msgs::Twist twist;
 
 public:
     VelocityConversion() : nh_("~"), rate(30)
     {
-        // Set up ROS connections
-        velocity_sub = nh_.subscribe("/velocity_node/velocity", 10, &VelocityConversion::velocityCallback, this);
-        steering_angle_sub = nh_.subscribe("/steering_node/filtered_angle", 10, &VelocityConversion::angleCallback, this);
-        gear_sub = nh_.subscribe("/velocity_node/gear", 10, &VelocityConversion::gearCallback, this);
-        twist_pub = nh_.advertise<geometry_msgs::TwistWithCovarianceStamped>("twist", 10);
-        nh_.param<double>("publish_frequency", publish_frequency, 30);
-        rate = ros::Rate(publish_frequency);
-        nh_.param<std::string>("base_link_frame", twist_frame, "base_link");
-
         // Set parameters
         nh_.param<double>("forklift/body/length", forklift_length, 2.5601);
         nh_.param<double>("length_to_base", length_to_base, 0); // length from the middle of the front axle to the base_link origin
+        nh_.param<std::string>("base_link_frame", twist_frame, "base_link");
+        nh_.param<double>("publish_frequency", publish_frequency, 30);
+        nh_.param<bool>("use_covariance", use_covariance, true);
+
+        // Set up ROS connections
+        velocity_sub = nh_.subscribe("/velocity_node/velocity", 10, &VelocityConversion::velocityCallback, this);
+        steering_angle_sub = nh_.subscribe("/steering_node/filtered_angle", 10, &VelocityConversion::angleCallback, this);
+
+        if (use_covariance) {
+            twist_with_cov_pub = nh_.advertise<geometry_msgs::TwistWithCovarianceStamped>("twist", 10);
+        }
+        else {
+            twist_pub = nh_.advertise<geometry_msgs::Twist>("twist", 10);
+        }
+
+        rate = ros::Rate(publish_frequency);
 
         // Set initial state
         velocity = 0;
         angle = 0;
-        //FIXME: starts in forward position right now, will need to adjust once we have control over the gear switch.
-        gear = 1;
 
         // Wait to receive one of each message type before continuing
         boost::shared_ptr<std_msgs::Float64 const> msg_ptr;
@@ -66,45 +73,60 @@ public:
         velocity = msg_ptr->data;
     }
 
-    void spin()
-    {
-        while (ros::ok()) {
-            // Uupdate twist message and publish
-            calculateTwist(twist);
-            twist_pub.publish(twist);
-            ros::spinOnce();
-            rate.sleep();
-        }
-    }
-
     void velocityCallback(const std_msgs::Float64 msg)
     {
         velocity = msg.data;
+
+        if (use_covariance) {
+            calculateTwistWithCov(twist_with_cov);
+            twist_with_cov_pub.publish(twist_with_cov);
+        }
+        else {
+            calculateTwist(twist);
+            twist_pub.publish(twist);
+        }
     }
 
     void angleCallback(const std_msgs::Float64 msg)
     {
         angle = msg.data;
+
+        if (use_covariance) {
+            calculateTwistWithCov(twist_with_cov);
+            twist_with_cov_pub.publish(twist_with_cov);
+        }
+        else {
+            calculateTwist(twist);
+            twist_pub.publish(twist);
+        }
     }
 
-    void gearCallback(const std_msgs::Int8 msg)
-    {
-        gear = msg.data;
-    }
-
-    void calculateTwist(geometry_msgs::TwistWithCovarianceStamped &forklift_twist)
+    void calculateTwistWithCov(geometry_msgs::TwistWithCovarianceStamped &forklift_twist)
     {
         // FIXME: try increasing the velocity value when turning
         //velocity *= 1 + (angle/M_PI);
 
         // Calculate the linear velocity at the base_link
         double v_b = velocity*sqrt(1 + pow((length_to_base/forklift_length)*tan(angle), 2));
-        double theta_dot = -gear*(velocity*(tan(angle)/forklift_length));
+        double theta_dot = (velocity*(tan(angle)/forklift_length));
 
         forklift_twist.header.stamp = ros::Time::now();
         forklift_twist.header.frame_id = twist_frame.c_str();
         forklift_twist.twist.twist.linear.x = v_b;
         forklift_twist.twist.twist.angular.z = theta_dot;
+    }
+
+    void calculateTwist(geometry_msgs::Twist &forklift_twist)
+    {
+        // FIXME: try increasing the velocity value when turning
+        //velocity *= 1 + (angle/M_PI);
+
+        // Calculate the linear velocity at the base_link
+        double v_b = velocity*sqrt(1 + pow((length_to_base/forklift_length)*tan(angle), 2));
+        double theta_dot = (velocity*(tan(angle)/forklift_length));
+
+        forklift_twist.linear.x = v_b;
+        forklift_twist.angular.z = theta_dot;
     }
 };
 
@@ -112,7 +134,7 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "velocity_conversion");
     VelocityConversion velocity_conversion;
-    velocity_conversion.spin();
+    ros::spin();
 
     return 0;
 }
